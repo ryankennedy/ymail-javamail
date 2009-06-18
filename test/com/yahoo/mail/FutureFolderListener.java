8fbb1a4b9defac90ded2abb5f89b81b80085d2d8
Ryan Kennedy <rckenned@yahoo.com>
2009-06-17 21:00:20 -0700

    Replaced the Thread.sleep() call in the folder listener test with some java.util.concurrent.Future<T> usage, preventing the test from consuming 5 seconds of time on every run. Created a FutureFolderListener test class, allowing tests to wait on a future event for a particular folder event type.

---
+++ test/com/yahoo/mail/FutureFolderListener.java
@@ -0,0 +1,70 @@
+package com.yahoo.mail;
+
+import javax.mail.event.FolderListener;
+import javax.mail.event.FolderEvent;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class FutureFolderListener implements FolderListener, Future<FolderEvent> {
+    private int eventType;
+    private FolderEvent event;
+    private boolean done;
+
+    public FutureFolderListener(int eventType) {
+        this.eventType = eventType;
+        this.done = false;
+    }
+
+    public void folderCreated(FolderEvent folderEvent) {
+        checkEvent(folderEvent);
+    }
+
+    public void folderDeleted(FolderEvent folderEvent) {
+        checkEvent(folderEvent);
+    }
+
+    public void folderRenamed(FolderEvent folderEvent) {
+        checkEvent(folderEvent);
+    }
+
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        return false;
+    }
+
+    public boolean isCancelled() {
+        return false;
+    }
+
+    public synchronized boolean isDone() {
+        return done;
+    }
+
+    public synchronized FolderEvent get() throws InterruptedException, ExecutionException {
+        while(!done) {
+            wait();
+        }
+        return event;
+    }
+
+    public synchronized FolderEvent get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        if(!done) {
+            wait(unit.toMillis(timeout));
+        }
+
+        if(!done) {
+            throw new TimeoutException("Timeout waiting for folder event");
+        }
+
+        return event;
+    }
+
+    private synchronized void checkEvent(FolderEvent folderEvent) {
+        if(folderEvent.getType() == eventType) {
+            done = true;
+            event = folderEvent;
+            this.notifyAll();
+        }
+    }
+}
--- test/com/yahoo/mail/StoreTest.java
+++ test/com/yahoo/mail/StoreTest.java
@@ -5,10 +5,8 @@ import org.junit.Test;
 import javax.mail.Store;
 import javax.mail.Folder;
 import javax.mail.MessagingException;
-import javax.mail.event.FolderListener;
 import javax.mail.event.FolderEvent;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import junit.framework.TestCase;
 
@@ -35,47 +33,43 @@ public class StoreTest {
             TestCase.fail("Failed to clean up test mailbox prior to testing");
         }
 
-        final List<String> created = new ArrayList<String>();
-        final List<String> deleted = new ArrayList<String>();
-        final List<String> renamed = new ArrayList<String>();
+        FutureFolderListener createdListener = new FutureFolderListener(FolderEvent.CREATED);
+        store.addFolderListener(createdListener);
 
-        FolderListener listener = new FolderListener() {
-            public void folderCreated(FolderEvent folderEvent) {
-                created.add(folderEvent.getFolder().getName());
-            }
+        FutureFolderListener renamedListener = new FutureFolderListener(FolderEvent.RENAMED);
+        store.addFolderListener(renamedListener);
 
-            public void folderDeleted(FolderEvent folderEvent) {
-                deleted.add(folderEvent.getFolder().getName());
-            }
-
-            public void folderRenamed(FolderEvent folderEvent) {
-                renamed.add(folderEvent.getNewFolder().getName());
-            }
-        };
-
-        store.addFolderListener(listener);
+        FutureFolderListener deletedListener = new FutureFolderListener(FolderEvent.DELETED);
+        store.addFolderListener(deletedListener);
 
         try {
             store.getFolder("added").create(Folder.HOLDS_MESSAGES);
             store.getFolder("added").renameTo(store.getFolder("renamed"));
             store.getFolder("renamed").delete(true);
 
-            // TODO: Replace with some of the Java futures stuff so we don't have to sleep.
+            FolderEvent created = null;
+            FolderEvent renamed = null;
+            FolderEvent deleted = null;
             try {
-                // Sleep for 5 seconds to wait for all the events to be delivered to the listeners.
-                Thread.sleep(5000);
+                created = createdListener.get(50, TimeUnit.MILLISECONDS);
+                renamed = renamedListener.get(50, TimeUnit.MILLISECONDS);
+                deleted = deletedListener.get(50, TimeUnit.MILLISECONDS);
             }
-            catch(InterruptedException e) { }
+            catch(Exception e) {
+                TestCase.fail("Exception waiting for folder events: " + e.toString());
+            }
 
-            TestCase.assertTrue("FolderListener.folderCreated() doesn't appear to have fired: " + created, created.contains("added"));
-            TestCase.assertTrue("FolderListener.folderRenamed() doesn't appear to have fired: " + renamed, renamed.contains("renamed"));
-            TestCase.assertTrue("FolderListener.folderDeleted() doesn't appear to have fired: " + deleted, deleted.contains("renamed"));
+            TestCase.assertEquals("FolderListener.folderCreated() doesn't appear to have fired", "added", created.getFolder().getName());
+            TestCase.assertEquals("FolderListener.folderRenamed() doesn't appear to have fired", "renamed", renamed.getNewFolder().getName());
+            TestCase.assertEquals("FolderListener.folderDeleted() doesn't appear to have fired", "renamed", deleted.getFolder().getName());
         }
         catch(MessagingException e) {
             TestCase.fail("Failed folder operation: " + e.toString());
         }
         finally {
-            store.removeFolderListener(listener);
+            store.removeFolderListener(createdListener);
+            store.removeFolderListener(renamedListener);
+            store.removeFolderListener(deletedListener);
             try {
                 store.close();
             }
@@ -87,7 +81,7 @@ public class StoreTest {
 
     @Test
     public void addStoreListener() {
-
+        
     }
 
     @Test
